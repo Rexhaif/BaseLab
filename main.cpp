021246085deb3cfbb973a2f31fb12160dcf3b3a3
Daniil Larionov <rexhaif.io@gmail.com>
2016-10-13 17:47:09 +0000

    working palindrome task

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 
+
 int main() {
-    long loli[] = {2, 2, 3, 3, 4, 4, 56, 5, 9};
-    unsigned it = 0;
-    cool_guys_hate_this : {
-        if (it >= (sizeof(loli) / sizeof(int))) goto exit;
-        std::cout << *(loli + it) << '|';
-        it++;
+
+    char const * string = "Kaaaksaak";
+
+    unsigned size = 0;
+    for(const char *p = string; *p != '\0'; ++p) {
+        size++;
     }
-    goto cool_guys_hate_this;
-    exit : {
-        return 0;
+
+    std::cout << "String length is: " << size << std::endl;
+    bool isPal = true;
+    for (int i = 0; i < size; ++i) {
+        std::cout << "Comparing chars at |"
+                  << i << "| and |" << (size-i) - 1 << "| positions (|"
+                  << char(tolower(string[i])) << "| and |"
+                  << char(tolower(string[(size-i) - 1])) << "|)"
+                  << " result : " << ((tolower(string[i]) != tolower(string[(size-i) - 1])) ? "not equal, breaking..." : "equal, continue...")
+                  << std::endl;
+        if (tolower(string[i]) != tolower(string[(size-i) - 1])) {
+            isPal = false;
+            break;
+        }
     }
+    if(isPal) {
+        std::cout << "This is Palindrome";
+    } else {
+        std::cout << "This is not palindrome";
+    }
+
+    return 0;
 }
