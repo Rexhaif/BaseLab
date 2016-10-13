747e68d9bfe5fafde823a8d24bf80d02c4f5697b
Daniil Larionov <rexhaif.io@gmail.com>
2016-10-13 17:59:05 +0000

    Raw testing info version

--- main.cpp
+++ main.cpp
@@ -2,31 +2,30 @@
 
 
 int main() {
-
+    /**
+     * String to test their "palindromity"
+     */
     char const * string = "Kaaaksaak";
 
-    std::cout << "|Palindrome task|" << std::endl
-              << "Testing string : " << string
-              << std::endl;
+    /**
+     * Calculating size of string
+     */
     unsigned size = 0;
     for(const char *p = string; *p != '\0'; ++p) {
         size++;
     }
 
-    std::cout << "String length is: " << size << std::endl;
+    /**
+     * Bi-directed chars comparing
+     */
     bool isPal = true;
     for (int i = 0; i < size; ++i) {
-        std::cout << "Comparing chars at |"
-                  << i << "| and |" << (size-i) - 1 << "| positions (|"
-                  << char(tolower(string[i])) << "| and |"
-                  << char(tolower(string[(size-i) - 1])) << "|)"
-                  << " result : " << ((tolower(string[i]) != tolower(string[(size-i) - 1])) ? "not equal, breaking..." : "equal, continue...")
-                  << std::endl;
         if (tolower(string[i]) != tolower(string[(size-i) - 1])) {
             isPal = false;
             break;
         }
     }
+
     if(isPal) {
         std::cout << "This is Palindrome";
     } else {
